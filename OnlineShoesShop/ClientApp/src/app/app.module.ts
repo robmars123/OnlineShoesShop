72a8daab70d869cddf3c381dab37d4b8064c075a
robmars123 <robmars2008@gmail.com>
2021-10-30 01:19:08 -0500

    Added homepage
    Added homepage also in the app.module

--- OnlineShoesShop/ClientApp/src/app/app.module.ts
+++ OnlineShoesShop/ClientApp/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomepageComponent, pathMatch: 'full' },
+      //Add more components here
+      // { path: 'home', component:  HomeComponent},
+      // { path: 'login', component:  LoginpageComponent},
+      // { path: 'assets', component: AssetsComponent },
+     // { path: 'assets/:id', component: AssetDetailsComponent }, //":id" colon before Id is important.
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
